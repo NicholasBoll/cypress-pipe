6331a9128e99fbb97b4a10d8a16226de30814e94
nico bellante <nicobell@microsoft.com>
2019-07-12 23:05:17 -0700

    add null as an allowed type

--- index.d.ts
+++ index.d.ts
@@ -8,10 +8,10 @@ declare namespace Cypress {
      * a `.should` AND returns a value synchronously (no `cy` commands inside).
      * In this instance, your function should not have any side effects and could be run
      * many times per second.
-     * 
+     *
      * ```ts
      * const getChildren = $el => $el.children()
-     * 
+     *
      * cy.get('body')
      *   .pipe(getChildren)
      *   .should('have.length', 3)
@@ -21,7 +21,7 @@ declare namespace Cypress {
       fn: (this: { [key: string]: any }, currentSubject: Subject) => Chainable<S> | Promise<S>,
       options?: Partial<Timeoutable & Loggable>,
     ): Chainable<S>
-    pipe<S extends object | any[] | string | number | boolean | undefined>(
+    pipe<S extends object | any[] | string | number | boolean | undefined | null>(
       fn: (this: { [key: string]: any }, currentSubject: Subject) => S,
       options?: Partial<Timeoutable & Loggable>,
     ): Chainable<S>
